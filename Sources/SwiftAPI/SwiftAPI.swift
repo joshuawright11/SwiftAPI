458fe0f9bfee06b60186f03e07d7b9ba97f16b54
Josh Wright <joshuawright11@gmail.com>
2019-11-23 11:49:46 -0800

    Add first crack at files

--- Sources/SwiftAPI/SwiftAPI.swift
+++ Sources/SwiftAPI/SwiftAPI.swift
@@ -1,3 +1,67 @@
-struct SwiftAPI {
-    var text = "Hello, World!"
+public struct Endpoint<Req: Codable, Res: Codable> {
+    public var path: String
+    public var method: HTTPMethod
+}
+
+public enum HTTPMethod {
+    case post, put, get, delete, patch
+}
+
+@propertyWrapper
+public class Method<Req: Codable, Res: Codable> {
+    public var wrappedValue: Endpoint<Req, Res>
+
+    init(_ method: HTTPMethod, _ path: String) {
+        self.wrappedValue = Endpoint<Req, Res>(path: path, method: method)
+    }
+}
+
+@propertyWrapper
+public class GET<Req: Codable, Res: Codable>: Method<Req, Res> {
+    // https://forums.swift.org/t/is-it-allowed-to-inherit-a-property-wrapper-class/28695
+    public override var wrappedValue: Endpoint<Req, Res> { didSet {} }
+    
+    public init(_ path: String) {
+        super.init(.get, path)
+    }
+}
+
+@propertyWrapper
+public class POST<Req: Codable, Res: Codable>: Method<Req, Res> {
+    // https://forums.swift.org/t/is-it-allowed-to-inherit-a-property-wrapper-class/28695
+    public override var wrappedValue: Endpoint<Req, Res> { didSet {} }
+
+    public init(_ path: String) {
+        super.init(.get, path)
+    }
+}
+
+@propertyWrapper
+public class PUT<Req: Codable, Res: Codable>: Method<Req, Res> {
+    // https://forums.swift.org/t/is-it-allowed-to-inherit-a-property-wrapper-class/28695
+    public override var wrappedValue: Endpoint<Req, Res> { didSet {} }
+
+    public init(_ path: String) {
+        super.init(.get, path)
+    }
+}
+
+@propertyWrapper
+class PATCH<Req: Codable, Res: Codable>: Method<Req, Res> {
+    // https://forums.swift.org/t/is-it-allowed-to-inherit-a-property-wrapper-class/28695
+    public override var wrappedValue: Endpoint<Req, Res> { didSet {} }
+
+    public init(_ path: String) {
+        super.init(.get, path)
+    }
+}
+
+@propertyWrapper
+class DELETE<Req: Codable, Res: Codable>: Method<Req, Res> {
+    // https://forums.swift.org/t/is-it-allowed-to-inherit-a-property-wrapper-class/28695
+    public override var wrappedValue: Endpoint<Req, Res> { didSet {} }
+
+    public init(_ path: String) {
+        super.init(.get, path)
+    }
 }

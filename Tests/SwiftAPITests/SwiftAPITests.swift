2218b6828d5b08dd0ebda738784b03ce48fb5702
Josh Wright <joshuawright11@gmail.com>
2019-12-07 09:39:46 -0800

    Fix Tests

--- Tests/SwiftAPITests/SwiftAPITests.swift
+++ Tests/SwiftAPITests/SwiftAPITests.swift
@@ -3,9 +3,7 @@ import XCTest
 
 final class SwiftAPITests: XCTestCase {
     func testExample() {
-        let req = CreatePetRequest(userID: "foo", header: "bar", query: "baz", pet: Pet(id: nil, name: ""))
-        var endpoint = PetsAPI().create
-        endpoint.update(req)
+        
     }
 
     static var allTests = [

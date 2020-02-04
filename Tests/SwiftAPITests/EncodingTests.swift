1c29a159790ebcd163c0c942f1e73c5683b3580e
Josh Wright <joshuawright@lyft.com>
2020-02-04 14:22:23 -0800

    Fix test

--- Tests/SwiftAPITests/EncodingTests.swift
+++ Tests/SwiftAPITests/EncodingTests.swift
@@ -2,45 +2,20 @@ import XCTest
 @testable import SwiftAPI
 
 final class EncodingTests: XCTestCase {
-//    let request = MockRequest()
-
     func testEncodeValue() throws {
-//        let testObj = TestObj(thing: "value")
-//
-//        let reqDTO = TestReqDTO(userID: "1234", number: 1, someThings: [], value: "test", obj: testObj)
-//        try reqDTO.encode(to: self.request, basePath: "/v1/shtuff/{userID}", method: .post)
-//        XCTAssert(self.request.path.hasPrefix("/v1/shtuff/1234"))
-//        XCTAssert(self.request.path.hasSuffix("?number=1"))
-//        XCTAssert(self.request.method == .post)
-//
-//        let testData = try testObj.toData()
-//        XCTAssert(self.request.body == testData)
+        let testAPI = TestAPI()
+        let testObj = TestObj(thing: "value")
+        let reqDTO = TestReqDTO(userID: "1234", number: 1, someThings: [], value: "test", obj: testObj)
+        let params = try testAPI.test.parameters(dto: reqDTO)
+        XCTAssert(params.fullPath.hasPrefix("/v1/accounts/1234/transfer"))
+        XCTAssert(params.fullPath.hasSuffix("?number=1"))
+        XCTAssert(params.method == .post)
+
+        let testData = try testObj.toData()
+        XCTAssert(params.body == testData)
     }
 
     static var allTests = [
         ("encodeValue", testEncodeValue),
     ]
 }
-
-//final class MockRequest: RequestEncoder {
-//    var body: Data?
-//    var headers: [String: String] = [:]
-//    var path: String = ""
-//    var method: HTTPMethod = .get
-//
-//    func setHeaders(_ headers: [String : String]) {
-//        self.headers = headers
-//    }
-//
-//    func setPath(_ path: String) {
-//        self.path = path
-//    }
-//
-//    func setBody(_ data: Data?) {
-//        self.body = data
-//    }
-//
-//    func setMethod(_ method: HTTPMethod) {
-//        self.method = method
-//    }
-//}

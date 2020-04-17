6b92df0f26783d462272ead9fcaa406bd3c1c1c4
Josh Wright <joshuawright11@gmail.com>
2020-04-17 15:05:52 -0700

    Add custom decoder

--- Sources/Vapor/API+Vapor.swift
+++ Sources/Vapor/API+Vapor.swift
@@ -5,28 +5,34 @@ import Vapor
 public struct API {
     /// The baseURL of all endpoints this requests
     let baseURL: String
+    let customDecoder: JSONDecoder?
     
     /// TODO add auth interceptors
-    public init(baseURL: String) {
+    public init(baseURL: String, customDecoder: JSONDecoder? = nil) {
         self.baseURL = baseURL
+        self.customDecoder = customDecoder
     }
     
     public func request<Req, Res>(_ endpoint: Endpoint<Req, Res>, _ dto: Req, _ client: Client) throws
         -> EventLoopFuture<(content: Res, response: ClientResponse)>
     {
         let parameters = try endpoint.parameters(dto: dto)
-        return client.performRequest(baseURL: self.baseURL, parameters)
+        return client.performRequest(baseURL: self.baseURL, parameters, customDecoder: self.customDecoder)
     }
     
     public func request<Res>(_ endpoint: Endpoint<SwiftAPI.Empty, Res>, _ client: Client)
         -> EventLoopFuture<(content: Res, response: ClientResponse)>
     {
-        client.performRequest(baseURL: self.baseURL, .just(url: endpoint.basePath, method: endpoint.method))
+        client.performRequest(baseURL: self.baseURL, .just(url: endpoint.basePath, method: endpoint.method),
+                              customDecoder: self.customDecoder)
     }
 }
 
 private extension Client {
-    func performRequest<Response: Codable>(baseURL: String, _ parameters: RequestParameters) -> EventLoopFuture<(content: Response, response: ClientResponse)> {
+    func performRequest<Response: Codable>(baseURL: String, _ parameters: RequestParameters,
+                                           customDecoder: JSONDecoder? = nil)
+        -> EventLoopFuture<(content: Response, response: ClientResponse)>
+    {
         let fullURL = baseURL + parameters.fullPath
         return self.send(parameters.method.nio, headers: HTTPHeaders(parameters.headers.map { $0 }),
                          to: URI(string: fullURL))
@@ -43,7 +49,11 @@ private extension Client {
                 throw Abort(response.status)
             }
             
-            return (try response.content.decode(Response.self), response)
+            if let customDecoder = customDecoder {
+                return (try response.content.decode(Response.self, using: customDecoder), response)
+            } else {
+                return (try response.content.decode(Response.self), response)
+            }
         }
     }
 }
